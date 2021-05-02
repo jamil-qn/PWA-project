18349de339f95cf996097ba04b8aff8c348959d2
jamil.qn <jamil.qolinezhad@gmail.com>
2021-05-02 16:24:15 +0430

    add the precache and dynamic cache for use offline mode

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,35 +1,68 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
+var sharedMomentsArea = document.querySelector("#shared-moments");
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  createPostArea.style.display = "block";
 }
 
-var installBtn = document.querySelector('#installApp-btn');
-var mainPage = document.querySelector('.main-page');
-installBtn.addEventListener('click' , () =>{
-  if(deferredPrompt){
+var installBtn = document.querySelector("#installApp-btn");
+var mainPage = document.querySelector(".main-page");
+installBtn.addEventListener("click", () => {
+  if (deferredPrompt) {
     deferredPrompt.prompt();
-    deferredPrompt.userChoice.then(choiceResuilt =>{
-      console.log("User choice: " , choiceResuilt.outcome);
-      if(choiceResuilt.outcome === "dismissed"){
-        console.log('User Cancelled installition');
-      } else{
-        console.log('User added app to home screen');
+    deferredPrompt.userChoice.then((choiceResuilt) => {
+      console.log("User choice: ", choiceResuilt.outcome);
+      if (choiceResuilt.outcome === "dismissed") {
+        console.log("User Cancelled installition");
+      } else {
+        console.log("User added app to home screen");
         installBtn.style.display = "none";
         mainPage.style.display = "block";
       }
 
       deferredPrompt = null;
-    })
+    });
   }
 });
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+
+function createCard() {
+  var cardWrapper = document.createElement("div");
+  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
+  var cardTitle = document.createElement("div");
+  cardTitle.className = "mdl-card__title";
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = "cover";
+  cardTitle.style.height = "180px";
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement("h2");
+  cardTitleTextElement.className = "mdl-card__title-text";
+  cardTitleTextElement.textContent = "San Francisco Trip";
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement("div");
+  cardSupportingText.className = "mdl-card__supporting-text";
+  cardSupportingText.textContent = "In San Francisco";
+  cardSupportingText.style.textAlign = "center";
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch("https://httpbin.org/get")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    createCard();
+  });
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,65 @@
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+
 self.addEventListener('install' , (event)=>{
     console.log('[service worker] Installing Service Worker...' , event);
+    event.waitUntil(caches.open(CACHE_STATIC_NAME)
+    .then((cache) =>{
+        console.log('[SW] Precaching App Shell');
+        cache.addAll([
+            '/',
+            '/index.html',
+            '/src/js/app.js',
+            '/src/js/feed.js',
+            '/src/js/promise.js',
+            '/src/js/fetch.js',
+            '/src/js/material.min.js',
+            '/src/css/app.css',
+            '/src/css/feed.css',
+            '/src/images/main-image.jpg',
+            '/favicon.ico',
+            'https://fonts.googleapis.com/css?family=Roboto:400,700',
+            'https://fonts.googleapis.com/icon?family=Material+Icons',
+            'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ]);
+    }));
 });
 
 self.addEventListener('activate' , (event)=>{
     console.log('[service worker] Activating Service Worker...' , event);
+    event.waitUntil(
+        caches.keys()
+        .then(keyList =>{
+            return Promise.all(keyList.map((key) =>{
+                if(key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME){
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
     return self.clients.claim();
 });
 
 self.addEventListener('fetch' , (event)=>{
-    console.log('[Service Worker] Fetching service worker...',event);
-    event.respondWith(fetch(event.request));
+    // console.log('[Service Worker] Fetching service worker...',event);
+    event.respondWith(
+        caches.match(event.request)
+        .then((response) =>{
+            if(response) {
+                return response;
+            }else {
+                return fetch(event.request)
+                .then((res) =>{
+                    caches.open(CACHE_DYNAMIC_NAME)
+                    .then((cache) =>{
+                        cache.put(event.request.url , res.clone());
+                        return res;
+                    })
+                })
+                .catch((err) =>{
+                    console.log(err);
+                })
+            }
+        })
+    );
 });
